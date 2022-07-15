8268e38aab7d9128043ac53e477fa3702e9658e3
kotyabuchi <kotyabuchi@gmail.com>
2022-07-16 03:40:43 +0900

    Add: イベント追加の基盤の実装

--- src/main/kotlin/com/github/kotyabuchi/mctd/MCTD.kt
+++ src/main/kotlin/com/github/kotyabuchi/mctd/MCTD.kt
@@ -1,13 +1,25 @@
 package com.github.kotyabuchi.mctd
 
+import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 class MCTD: JavaPlugin() {
 
+    private val events: List<Listener> = listOf(
+    )
+
+    private fun registerEvents() {
+        events.forEach { event ->
+            server.pluginManager.registerEvents(event, this)
+        }
+    }
+
     override fun onEnable() {
         setupKoin()
+        if (!dataFolder.exists()) dataFolder.mkdirs()
+        registerEvents()
     }
 
     override fun onDisable() {
